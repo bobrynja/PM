1e64fee06d4a7c4534dddac3157b6774ac11b8a1
stu-pksp120 <stu-pksp120@AMDVLA3511GW01>
2024-04-19 09:20:25 +0300

    Создание заполнения массива экземплярами класса

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,11 +9,42 @@ namespace ConsoleApp1
         public string age;
     }
 
+    class HR
+    {
+        public Person [] array;
+
+        public void Sort(Person [] a)
+        {
+            
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person now;
+            string last_name, name, age;
+            HR ar = new HR(); 
+            Console.WriteLine("Введите количество элементов массива: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            ar.array = new Person[i];
+            for(int j = 0; j < i; j++)
+            {
+                now = new Person();
+                Console.WriteLine($"Ввод {i} элемента: ");
+                Console.WriteLine("Введите фамилию: ");
+                now.last_name = Console.ReadLine();
+                Console.WriteLine("Введите имя: ");
+                now.name = Console.ReadLine();
+                Console.WriteLine("Введите возраст: ");
+                now.age = Console.ReadLine();
+                ar.array[j] = now;
+
+
+            }
+
         }
     }
 }
